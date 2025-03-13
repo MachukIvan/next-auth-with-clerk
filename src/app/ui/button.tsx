1988bbf936851d4025eecd804d9aa091e02dba63
Ivan Machuk <machuk.ivan@yandex.by>
2025-03-13 21:21:58 +0100

    Set up Clerk for authentication

---
+++ src/app/ui/button.tsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode;
+}
+
+export function Button({ children, className, ...rest }: ButtonProps) {
+    return (
+        <button
+            {...rest}
+            className={clsx(
+                'flex w-48 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
+                className
+            )}
+        >
+            {children}
+        </button>
+    );
+}
